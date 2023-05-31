bb24cee3034cad4fd21af4ae8899a02da3da01bb
laisrmacedo <laisrodriguesmacedo@gmail.com>
2023-05-31 23:07:01 +0200

    chore: installation of dependencies

--- barber-shop/src/App.js
+++ barber-shop/src/App.js
@@ -1,8 +1,42 @@
+import { Home } from "./components/Home";
+import { About } from "./components/About";
+import styled from "styled-components";
+import instagram from './assets/instagram.png'
+import whatsapp from './assets/whatsapp.png'
+
+const Container = styled.div`
+  height: 400vh;
+  width: 100vw;
+  display: flex;
+  align-items: center;
+  /* justify-content: center; */
+  flex-direction: column;
+  position: relative;
+  /* border: solid 2px yellow; */
+`
+const SocialMedia = styled.div`
+  height: 100px;
+  width: 25px;
+  display: flex;
+  gap: 10px;
+  flex-direction: column;
+  /* align-items: center; */
+  position: fixed;
+  bottom: 0px;
+  left: 90px;
+  /* border: solid 2px yellow; */
+`
+
 function App() {
   return (
-    <div >
-     
-    </div>
+    <Container >
+     <Home/>
+     <About/>
+     <SocialMedia>
+      <img src={instagram}/>
+      <img src={whatsapp}/>
+     </SocialMedia>
+    </Container>
   );
 }
 
